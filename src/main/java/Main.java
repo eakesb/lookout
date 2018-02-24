ca39129cfa89f67462e59ca86f105d20db1b0ba9
eakesb <bceakes@gmail.com>
2018-02-23 16:46:31 -0800

    configurable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,10 @@ import com.amazonaws.services.simpleemail.model.SendEmailResult;
 import com.google.common.base.Splitter;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
+import org.apache.commons.configuration.CompositeConfiguration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.SystemConfiguration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
@@ -19,6 +23,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Executors;
@@ -35,22 +41,27 @@ public class Main {
     private final ScheduledExecutorService executorService;
 
     private final ImmutableList<String> sites;
-    private final String sender = "bceakes@gmail.com";
-    private final String[] recipients = new String[]{sender};
+    private final String sender;
+    private final String[] recipients;
+    private final DateTime cutoff;
+    private final long periodMinutes;
 
-    public Main(String[] args) {
+    public Main(CompositeConfiguration config) {
         sesClient = AmazonSimpleEmailServiceClientBuilder.standard()
                 .withRegion(Regions.US_WEST_2)
                 .build();
 
         executorService = Executors.newSingleThreadScheduledExecutor();
-        sites = ImmutableList.of(
-                "Clear Lake/75097/146537"
-        );
+
+        sender = config.getString("sender", "bceakes@gmail.com");
+        recipients = config.getStringArray("recipients");
+        sites = ImmutableList.copyOf(config.getStringArray("lookouts"));
+        cutoff = DateTime.parse(config.getString("cutoff", "2018-06-01"), DateTimeFormat.forPattern("YYYY-MM-dd"));
+        periodMinutes = config.getLong("periodMinutes", 5);
     }
 
     private void start() {
-        executorService.scheduleAtFixedRate(this::run, 0, 30, TimeUnit.SECONDS);
+        executorService.scheduleAtFixedRate(this::run, 0, periodMinutes, TimeUnit.MINUTES);
     }
 
     private void shutdown() {
@@ -128,8 +139,8 @@ public class Main {
 
         DateTime dateTime = DateTime.parse(month + " " + day, DateTimeFormat.forPattern("MMM YYYY dd"));
 
-        if (dateTime.getMonthOfYear() >= 6) {
-            log.info(lookout + " not available before June");
+        if (!dateTime.isBefore(cutoff)) {
+            log.info(lookout + " not available before cutoff");
             return Optional.empty();
         }
 
@@ -139,8 +150,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Main main = new Main(args);
+        Main main = new Main(getConfiguration(args));
         Runtime.getRuntime().addShutdownHook(new Thread(main::shutdown));
         main.start();
+    }
+
+    private static CompositeConfiguration getConfiguration(String[] args) {
+        CompositeConfiguration compositeConfiguration = new CompositeConfiguration();
+        compositeConfiguration.addConfiguration(new SystemConfiguration());
+
+        if (args.length == 1) {
+            String configFilePath = args[0];
+            boolean exists = Files.exists(Paths.get(configFilePath));
+            if (!exists) {
+                System.err.println("Configuration file not found at '" + configFilePath + "");
+                System.exit(1);
+            }
+
+            try {
+                compositeConfiguration.addConfiguration(new PropertiesConfiguration(configFilePath));
+            } catch (ConfigurationException e) {
+                log.error("Failed to read configuration file at path: " + configFilePath, e);
+                System.exit(1);
+            }
+        }
+
+        return compositeConfiguration;
     }
 }
